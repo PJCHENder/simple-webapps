27bcde8ce3667fff6d1a5e2384f6c6ea41b4de52
pjchender <pjchender@gmail.com>
2017-07-14 09:31:35 +0800

    Use line to manipulate bot

--- routes/v1.0/linebot.js
+++ routes/v1.0/linebot.js
@@ -7,41 +7,37 @@ const lineConfig = require('../../config/line')
 const bot = linebot(lineConfig)
 const linebotParser = bot.parser()
 
-
 /**
  * Line Bot Listener
  */
 bot.on('message', function (event) {
-  console.log('event', event)
-  event.reply(event.message.text).then(function (data) {
+  if (event.message.text === '開始') {
+    event.reply('開始打怪').then(function (data) {
+      // success
+      task.start()
+      bot.push('U14a11d23e77aaad34a5e9893f0570223', 'Task Start')
+    }).catch(function (error) {
+      // error
+      console.error('error on message', error)
+    })
+  } else if (event.message.text === '結束') {
+    event.reply('停止打怪').then(function (data) {
     // success
-  }).catch(function (error) {
-    // error
-    console.error('error on message', error)
-  })
+      task.stop()
+      bot.push('U14a11d23e77aaad34a5e9893f0570223', 'Task Stop')
+    }).catch(function (error) {
+      // error
+      console.error('error on message', error)
+    })
+  }
 })
 
-let task = cron.schedule('50,55 1,5,9,13,17,21 * * *', function () {
+let task = cron.schedule('35,40,45,50,55 1,5,9,13,17,21 * * *', function () {
   bot.push('U14a11d23e77aaad34a5e9893f0570223', `該動起來了${new Date()}`)
   console.log('running a task', new Date())
 }, true)
 
 // POST /v1.0/linebot/webhook
 router.post('/webhook', linebotParser)
-
-let isTaskStart = true
-
-// GET /v1.0/linebot/
-router.get('/', (req, res, next) => {
-  if (isTaskStart) {
-    task.stop()
-    bot.push('U14a11d23e77aaad34a5e9893f0570223', 'Task Stop')
-  } else {
-    task.start()
-    bot.push('U14a11d23e77aaad34a5e9893f0570223', 'Task Start')
-  }
-
-  return res.status(200).json({status: 200, message: `isTaskStart: ${isTaskStart}`, time: new Date()})
-})
 
 module.exports = router
