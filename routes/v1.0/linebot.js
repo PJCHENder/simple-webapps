a06c0d3e3ef90b91eea9740df5aaf7586ff3e064
pjchender <pjchender@gmail.com>
2017-07-16 13:54:28 +0800

    Modify line Bot

--- routes/v1.0/linebot.js
+++ routes/v1.0/linebot.js
@@ -11,38 +11,39 @@ const linebotParser = bot.parser()
  * Line Bot Listener
  */
 bot.on('message', function (event) {
-  if (event.message.text === '開始打怪') {
-    event.reply('收到').then(function (data) {
-      // success
-      task.start()
-      bot.push('U14a11d23e77aaad34a5e9893f0570223', '微寶出任務')
-    }).catch(function (error) {
-      // error
-      console.error('error', error)
-    })
-  } else if (event.message.text === '停止打怪') {
-    event.reply('收到').then(function (data) {
-    // success
-      task.stop()
-      bot.push('U14a11d23e77aaad34a5e9893f0570223', '微寶結束任務')
-    }).catch(function (error) {
-      // error
-      console.error('error', error)
-    })
-  } else {
-    event.reply('這我不理解').then(function (data) {
-    // success
-    }).catch(function (error) {
-      // error
-      console.error('error', error)
-    })
-  }
+  console.log('event', event)
+  // if (event.message.text === '微寶覺醒') {
+  //   event.reply('收到').then(function (data) {
+  //     // success
+  //     task.start()
+  //     bot.push('U14a11d23e77aaad34a5e9893f0570223', '微寶出任務')
+  //   }).catch(function (error) {
+  //     // error
+  //     console.error('error', error)
+  //   })
+  // } else if (event.message.text === '微寶睡覺') {
+  //   event.reply('收到').then(function (data) {
+  //   // success
+  //     task.stop()
+  //     bot.push('U14a11d23e77aaad34a5e9893f0570223', '微寶結束任務')
+  //   }).catch(function (error) {
+  //     // error
+  //     console.error('error', error)
+  //   })
+  // } else {
+  //   event.reply('這我不理解').then(function (data) {
+  //   // success
+  //   }).catch(function (error) {
+  //     // error
+  //     console.error('error', error)
+  //   })
+  // }
 })
 
-let task = cron.schedule('50,55,59 1,5,9,13,17,21 * * *', function () {
-  bot.push('U14a11d23e77aaad34a5e9893f0570223', '該動起來了')
-  console.log('running a task', new Date())
-}, true)
+// let task = cron.schedule('50,55,59 1,5,9,13,17,21 * * *', function () {
+//   bot.push('U14a11d23e77aaad34a5e9893f0570223', '該動起來了')
+//   console.log('running a task', new Date())
+// }, true)
 
 // POST /v1.0/linebot/webhook
 router.post('/webhook', linebotParser)
